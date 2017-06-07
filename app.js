ab444691406a1cdef2e02bb51d8f9569f1b5b2a9
meowmeowcomputers <ryansimonleon@gmail.com>
2017-06-07 14:31:56 -0500

    Completed up to step 5: Search results link to pages

--- app.js
+++ app.js
@@ -24,3 +24,28 @@ app.listen(8003, function () {
 app.get('/', function (request, response) {
   response.render('home.hbs', {});
 });
+
+app.get('/search', function(request, response) {
+  var desc = request.query.desc
+  console.log(desc);
+  db.query(`SELECT * FROM restaurant WHERE name ILIKE '%${desc}%'`)
+    .then(function (dbresults) {
+      console.log(dbresults);
+      response.render('view.hbs', {title:'Results', dbresults: dbresults});
+      })
+})
+
+app.get('/restaurant/:id', function(request, response, next) {
+  let id = request.params.id;
+  db.query(`SELECT *, restaurant.name as resname FROM restaurant \
+    JOIN review ON restaurant.id = review.restaurant_id \
+    JOIN reviewer ON reviewer.id = review.reviewer_id WHERE restaurant.id = '${id}'`)
+  .then(function (results) {
+    console.log(results)
+    response.render('restaurant.hbs', {title:results.resname, results:results})
+  })
+  .catch(function rejected(err) {
+    console.log('error:', err.stack);
+    response.render(error)
+  })
+})
